be669366f9286fa797a7077dc81340ad4cd91f11
3A532092 <star1998410@gmail.com>
2018-10-26 18:00:19 +0800

    三頁的view

---
+++ app/Http/routes.php
@@ -0,0 +1,4 @@
+<?php
+Route::get('/', ['as'=>'home.index','uses'=>'HomeController@index']);
+Route::get('/about', ['as'=>'about.index','uses'=>'AboutController@index']);
+Route::get('/news', ['as'=>'news.index','uses'=>'NewsController@index']);
